54cae1e33954635399a873a29bda0f2de157fb3f
rin <neolithium.redux@gmail.com>
2022-04-22 03:52:24 +0700

    added help command & bot activity.

--- mihate.py
+++ mihate.py
@@ -16,14 +16,18 @@ token = input("Enter bot token: ")
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="%help"))
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith(prefix + "greet"):
-        await message.channel.send("Hello! I'm Mihate")
+    if message.content.startswith(prefix + "help"):
+        await message.channel.send("**Mihate** Commands: \n")
+        await message.channel.send("\n`" + prefix + "greet` - greets the user")
+    elif message.content.startswith(prefix + "greet"):
+        await message.channel.send("Hello, I'm Mihate Hiura!")
         await message.channel.send("https://cloud.neoservices.xyz/f/97138729272743b595af/?raw=1")
 
 # dev token
