743483a1b9aee3d75e1509ce88f82f55cc5a255c
rin <neolithium.redux@gmail.com>
2022-04-25 11:22:05 +0700

    reimplemented commands properly. added user changeable prefix

--- mihate.py
+++ mihate.py
@@ -1,16 +1,17 @@
 import discord
 import logging
 import os
-from random import seed
-from random import randint
+
+from discord.ext import commands
 
 logging.basicConfig(level=logging.INFO)
 
-client = discord.Client()
+client = commands.Bot(command_prefix="%")
 
-prefix = "%"
-#prefix = (os.environ['PREFIX'])
+# prefix = (os.environ['PREFIX'])
 token = input("Enter bot token: ")
+
+# on-ready console notification & bot presence
 
 
 @client.event
@@ -18,17 +19,26 @@ async def on_ready():
     print("Logged in as {0.user}".format(client))
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="%help"))
 
+# greet command
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith(prefix + "help"):
-        await message.channel.send("**Mihate** Commands: \n")
-        await message.channel.send("\n`" + prefix + "greet` - greets the user")
-    elif message.content.startswith(prefix + "greet"):
-        await message.channel.send("Hello, I'm Mihate Hiura!")
-        await message.channel.send("https://cloud.neoservices.xyz/f/97138729272743b595af/?raw=1")
+
+@client.command(
+    help="Greets the user"
+)
+async def greet(ctx):
+    await ctx.channel.send("Hello, I'm Mihate Hiura!")
+    await ctx.channel.send("https://cloud.neoservices.xyz/f/97138729272743b595af/?raw=1")
+
+# change prefix command
+
+
+@client.command(
+    help="Change the prefix of the bot"
+)
+async def prefix(ctx, prefArg):
+    dispNP = "The new bot prefix is " + prefArg
+    await ctx.channel.send(dispNP)
+    client.command_prefix = prefArg
 
 # dev token
 client.run(token)
