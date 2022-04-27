76117430b1e074e75a49e778e76f93a2e43af685
rin <neolithium.redux@gmail.com>
2022-04-27 13:40:17 +0700

    added random image command. assets added

--- mihate.py
+++ mihate.py
@@ -2,6 +2,8 @@ import discord
 import logging
 import os
 from art import *
+from random import *
+import random
 
 from discord.ext import commands
 
@@ -11,7 +13,6 @@ client = commands.Bot(command_prefix="%")
 
 # prefix = (os.environ['PREFIX'])
 token = input("Enter bot token: ")
-
 
 # on-ready console notification & bot presence
 @client.event
@@ -23,14 +24,12 @@ async def on_ready():
         activity=discord.Activity(type=discord.ActivityType.listening,
                                   name=(client.command_prefix + "help")))
 
-
 # greet command
 @client.command(help="Greets the user")
 async def greet(ctx):
     await ctx.channel.send("Hello, I'm Mihate Hiura!")
     await ctx.channel.send(
         "https://cloud.neoservices.xyz/f/97138729272743b595af/?raw=1")
-
 
 # change prefix command
 @client.command(help="Change the prefix of the bot")
@@ -42,12 +41,46 @@ async def prefix(ctx, prefArg):
         activity=discord.Activity(type=discord.ActivityType.listening,
                                   name=(client.command_prefix + "help")))
 
-
 # random lineart command
 @client.command(help="Sends a random ASCII line art")
 async def lineart(ctx):
     await ctx.channel.send(randart())
 
+# random hiura image
+@client.command(help="Sends an image of Hiura with randomized rarity")
+async def imageroll(ctx):
+    seed(109)
+    rng = randint(0,1000)
+    if(0<rng<599):
+            common = './assets/Common/'
+            cList = os.listdir(common)
+            cRNG = random.choice(cList)
+            cPath = common+cRNG;
+            await ctx.channel.send(file=discord.File(cPath))
+    elif(600<rng<850):
+            common = './assets/Elite/'
+            eList = os.listdir(common)
+            eRNG = random.choice(cList)
+            ePath = common+cRNG;
+            await ctx.channel.send(file=discord.File(ePath))
+    elif(851<rng<980):
+            common = './assets/Rare/'
+            rList = os.listdir(common)
+            rRNG = random.choice(cList)
+            rPath = common+cRNG;
+            await ctx.channel.send(file=discord.File(rPath))
+    elif(981<rng<998):
+            common = './assets/SSR/'
+            sList = os.listdir(common)
+            sRNG = random.choice(cList)
+            sPath = common+cRNG;
+            await ctx.channel.send(file=discord.File(sPath))
+    elif(999<rng<1000):
+            common = './assets/UR/'
+            uList = os.listdir(common)
+            uRNG = random.choice(cList)
+            uPath = common+cRNG;
+            await ctx.channel.send(file=discord.File(uPath))
 
 # dev token
 client.run(token)
