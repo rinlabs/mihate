5b95cf2258246717b7b97783fe407283f07aef73
rin <neolithium.redux@gmail.com>
2022-05-21 22:27:22 +0700

    added ipv4 fetching and filtering

--- modules/aegis/urlProcessing.py
+++ modules/aegis/urlProcessing.py
@@ -1,5 +1,9 @@
 from urlextract import URLExtract
 from urllib.parse import urlparse
+import dns
+import dns.resolver
+import re
+import iocextract
 
 def extractUrl(url):
     if (URLExtract().has_urls(urlCheck(url)) == True):
@@ -19,3 +23,30 @@ def urlCheck(url):
     else:
         # returns untouched url if contains http(s)
         return url
+
+def v4DNS(url):
+    v4 = []
+    try:
+        v4 = dns.resolver.resolve(url,'A')
+    except (dns.exception.Timeout):
+        print("DNS Query Timed Out")
+    except (dns.resolver.NoAnswer):
+        print("DNS Record No Answer")
+    except (dns.resolver.NXDOMAIN):
+        print("NXDOMAIN")
+    return v4
+
+def v4Regex(url):
+    v4 = []
+
+    for i in iocextract.extract_ipv4s(url):
+        v4.append(i)
+    return v4
+
+def getv4List(url):
+    v4 = []
+    for index,i in enumerate(extractUrl(url)):
+        for i in (v4DNS(extractUrl(url)[index])):
+            v4.append(i)
+    v4.extend(v4Regex(url))
+    return v4
