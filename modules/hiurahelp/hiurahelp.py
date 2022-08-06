5b41184898f81fe6689cab016825066fcc8a3247
rinlabs <neolithium.redux@gmail.com>
2022-08-07 00:20:49 +0700

    updated help embed

--- modules/hiurahelp/hiurahelp.py
+++ modules/hiurahelp/hiurahelp.py
@@ -2,31 +2,36 @@ import discord
 
 # creates embed
 def helpEmbed(prefix):
-    embed = discord.Embed(color=0xFFFFFF)
+    embed = discord.Embed(color=0xFF0064)
     embed.add_field(name="__**Hiuraroll**__", value="Spawns a random Hiura"
                               + "\n\n"
                               + "Commands to type: "
                               + "\n"
-                              + "**"+prefix+"hiuraroll** \n",
+                              + "**"+prefix+"hiuraroll**",
                               inline=False)
-    embed.add_field(name="\n__**Hiura Greet**__", value="Greets the user"
+    embed.add_field(name="__**Hiura Greet**__", value="Greets the user"
                               + "\n\n"
                               + "Commands to type: "
                               + "\n"
-                              + "**"+prefix+"greet** \n",
+                              + "**"+prefix+"greet**",
                               inline=False)
-    embed.add_field(name="\n__**Lineart**__", value="Spawns a random lineart"
+    embed.add_field(name="__**Lineart**__", value="Spawns a random lineart"
                               + "\n\n"
                               + "Commands to type: "
                               + "\n"
-                              + "**"+prefix+"lineart** \n",
+                              + "**"+prefix+"lineart**",
                               inline=False)
-    
-    embed.add_field(name="\n__**Help**__", value="Invokes this message"
+    embed.add_field(name="__**Coinflip**__", value="Flips a coin"
                               + "\n\n"
                               + "Commands to type: "
                               + "\n"
-                              + "**"+prefix+"help** \n",
+                              + "**"+prefix+"coinflip**",
+                              inline=False)   
+    embed.add_field(name="__**Help**__", value="Invokes this message"
+                              + "\n\n"
+                              + "Commands to type: "
+                              + "\n"
+                              + "**"+prefix+"help**",
                               inline=False)
     embed.set_author(name="Mihate Hiura")
     embed.set_footer(text="© Rinlabs 2022")
